c53e296930bef041eb408e5f5e65fac748941230
CrawlFu <furui_2016@163.com>
2017-10-30 17:28:25 +0800

    ok

---
+++ FenWeiBo/FenWeiBo/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FenWeiBo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            List<string> asa = new List<string>();
+            asa.Add("sss");
+            asa.Add("sdfa");
+            ViewBag.List = asa;
+            return View();
+        }
+    }
+}
---
+++ FenWeiBo/FenWeiBo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FenWeiBo.MvcApplication" Language="C#" %>
